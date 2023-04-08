4e93d5f6204bf773c99b0a692af6f36a68d700b5
Siddharth pandey <44026219+sid288791@users.noreply.github.com>
2023-04-08 11:02:44 +0530

    first commit

---
+++ src/main/java/com/sid/pgpcipher/PgpcipherJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sid.pgpcipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgpcipherJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgpcipherJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sid/pgpcipher/PgpcipherJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sid.pgpcipher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgpcipherJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
